4bd8b71337210338e60fa9c4011e617b55e63d06
Foundling <jakobi.hammonds@stu.evsck12.com>
2023-11-30 09:24:36 -0600

    update

---
+++ PythonMaterial/Notes/CSEUnit3/IODATA/HowdyDoody.py
@@ -0,0 +1,7 @@
+Name = input("What is your name?"+ "") # gets input
+#print("Howdy " +name+ "!") # Output to Terminal
+print(f"Howdy {name}!") # Inject Variables
+
+print(f"Welcome to our great {name}venture! where {name} is the adventure!")
+
+age = input(f"{name} What is your age?")
